398a2e3a3ac4573ab8ed6bcc862808bbfc37c129
travisgallagher <travisgallagher52@gmail.com>
2022-02-09 16:27:56 -0700

    Questions 1-10 complete.

--- lab.js
+++ lab.js
@@ -78,7 +78,7 @@ if(temperature >= 80 && rain === true) {
   console.log("Wear a t-shirt and take an umbrella.");
 } else if(temperature > 60 && temperature < 80 && rain === true) {
   console.log("Wear a rain-jacket.");
-} else if(temperature <= 60 && rain == true) {
+} else if(temperature <= 60 && rain === true) {
   console.log("Wear a jacket and carry an umbrella.");
 
 } else if(temperature >= 80 && rain === false) {
@@ -160,27 +160,35 @@ while(score < passingScore) {
 }
 
 
-
-
-
-
-
-
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
 /*
   Create a variable called 'changeMyMind' and set it equal to true. 
-  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
-
+  Check to see if changeMyMind is set to true or false, if it is true, 
+  change the status to false, if it is false, change the status to true.
 */
 
 //CODE HERE
+let changeMyMind = true; 
+
+if(changeMyMind === true){
+  changeMyMind = false; 
+} else if(changeMyMind === false) {
+  changeMyMind = true; 
+}
 
 ////////// PROBLEM 10 //////////
-// Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
+// Try to change the value of changeMyMind (irrespective of whether it is true or false) 
+// and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
+
+if(changeMyMind === true){
+  console.log(changeMyMind);
+} else {
+  console.log(!changeMyMind);
+}
 
 
 
@@ -189,6 +197,12 @@ while(score < passingScore) {
 ////////// PROBLEM 11 //////////
 
 let z = 5
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// Create a while loop that continues to run while z is greater than 0. 
+//Within the while loop, log a countdown from the value of z to 1. 
+//Once you have logged the countdown, make sure you decrement z by 1. 
+//If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+while(z > 0){
+ 
+}
